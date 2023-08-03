f0839df28602255cf8d6e7f8dde5c888da07c636
Gautham2980 <gautham4816@gmail.com>
2023-08-03 12:56:30 +0530

    datemethod

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var a=new Date();
+a.getDate();
+console.log(a);
+document.write(a+"<br>");
+
+var b=new Date();
+var hrs=b.getHours();
+console.log(hrs);
+document.write(hrs+"<br>");
+
+var c=new Date();
+var mon=c.getMonth();
+console.log(mon);
+document.write(mon+"<br>");  
+
+var d=new Date();
+var min=d.getMinutes();
+console.log(min);
+document.write(min+"<br>");
+
+var e=new Date();
+var year=e.getFullYear();
+console.log(year);
+document.write(year+"<br>");
